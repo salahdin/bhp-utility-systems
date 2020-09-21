4670249b197947051202b052de44d5231328e40c
ckgathi <ckgathi@users.noreply.github.com>
2020-09-21 11:34:35 +0200

    navbars

--- bhp_utility_systems/navbars.py
+++ bhp_utility_systems/navbars.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from edc_navbar import NavbarItem, site_navbars, Navbar
 
 
 bhp_utility_systems = Navbar(name='bhp_utility_systems')
+
+bhp_utility_systems.append_item()
 
 site_navbars.register(bhp_utility_systems)
